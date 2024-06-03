98e71b3cbd62ff1e511139057a520f07c96faf19
dniherdiana <86401493+dniherdiana@users.noreply.github.com>
2024-06-03 13:08:51 +0700

    .

--- src/data/MenuRender.jsx
+++ src/data/MenuRender.jsx
@@ -12,31 +12,29 @@ const MenuRender = ({ menuItems }) => {
           <h2>{item.strMeal}</h2>
           <p>{item.strTags}</p>
           <img src={item.strMealThumb} alt={item.strMeal} />
-          <iframe
-            src={item.strYoutube}
-            frameborder="0"
-            height="350"
-            width="500"
-          ></iframe>
+          <iframe src={item.strYoutube} height="350" width="500"></iframe>
           <p>{item.strInstructions}</p>
 
-          <ul>
-            {[...Array(20).keys()].map((i) => {
-              const ingredient = item[`strIngredient${i + 1}`];
-              const measure = item[`strMeasure${i + 1}`];
-              if (
-                // ingredient &&
-                // measure &&
-                // ingredient !== "" &&
-                // measure !== " "
-                ingredient &&
-                measure !== ""
-              ) {
-                return <li key={i}>{`${measure} - ${ingredient}`}</li>;
-              }
-              return null;
-            })}
-          </ul>
+          <div id="ingredients">
+            <h3>Ingredients:</h3>
+            <ul>
+              {[...Array(20).keys()].map((i) => {
+                const ingredient = item[`strIngredient${i + 1}`];
+                const measure = item[`strMeasure${i + 1}`];
+                if (
+                  // ingredient &&
+                  // measure &&
+                  // ingredient !== "" &&
+                  // measure !== " "
+                  ingredient &&
+                  measure !== ""
+                ) {
+                  return <li key={i}>{`${measure} - ${ingredient}`}</li>;
+                }
+                return null;
+              })}
+            </ul>
+          </div>
         </div>
       ))}
     </div>
